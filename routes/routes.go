9344db578e8990bdc772e3d1241629c3910e1b8f
zeimedee <42755623+zeimedee@users.noreply.github.com>
2023-10-18 14:10:52 +0000

    wip company,objective,key result models

--- routes/routes.go
+++ routes/routes.go
@@ -12,62 +12,62 @@ func DefaultHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": true})
 }
 
-func TestCreateHandler(c *gin.Context) {
-	var data models.Test
-	err := c.ShouldBind(&data)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"success": false})
-		return
-	}
-	database.CreateItem(&data)
-	c.JSON(http.StatusOK, gin.H{"success": data})
-}
+// func TestCreateHandler(c *gin.Context) {
+// 	var data models.Test
+// 	err := c.ShouldBind(&data)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"success": false})
+// 		return
+// 	}
+// 	database.CreateItem(&data)
+// 	c.JSON(http.StatusOK, gin.H{"success": data})
+// }
 
-func TestGetItemsHandler(c *gin.Context) {
-	var data []models.Test
-	_, err := database.GetAllItems(&data, "name", "dexter")
+// func TestGetItemsHandler(c *gin.Context) {
+// 	var data []models.Test
+// 	_, err := database.GetAllItems(&data, "name", "dexter")
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"success": true, "data": data})
-}
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"success": true, "data": data})
+// }
 
-func TestGetItemHandler(c *gin.Context) {
-	var data models.Test
-	_, err := database.GetItem(&data, "id", "1")
+// func TestGetItemHandler(c *gin.Context) {
+// 	var data models.Test
+// 	_, err := database.GetItem(&data, "id", "1")
 
-	if err != nil {
-		c.JSON(http.StatusBadGateway, gin.H{"success": "false"})
-		return
-	}
-	if data.ID == 0 {
-		c.JSON(http.StatusOK, gin.H{"success": true, "data": ""})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"success": true, "data": data})
-}
+// 	if err != nil {
+// 		c.JSON(http.StatusBadGateway, gin.H{"success": "false"})
+// 		return
+// 	}
+// 	if data.ID == 0 {
+// 		c.JSON(http.StatusOK, gin.H{"success": true, "data": ""})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"success": true, "data": data})
+// }
 
-func TestUpdateItemHandler(c *gin.Context) {
-	var data models.Test
-	err := database.UpdateItem(data, "id", "4", "name", "diego")
+// func TestUpdateItemHandler(c *gin.Context) {
+// 	var data models.Test
+// 	err := database.UpdateItem(data, "id", "4", "name", "diego")
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"success": false})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"success": true, "message": "updated successfully"})
-}
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"success": false})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "updated successfully"})
+// }
 
-func TestDeleteItemHandler(c *gin.Context) {
-	var data models.Test
+// func TestDeleteItemHandler(c *gin.Context) {
+// 	var data models.Test
 
-	err := database.DeleteItem(data, "id", "1")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"success": true, "message": "deleted successfully"})
+// 	err := database.DeleteItem(data, "id", "1")
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "deleted successfully"})
 
-}
+// }
