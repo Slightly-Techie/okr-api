4dea82e7a398b0224e1bd3101e077218585461ae
zeimedee <42755623+zeimedee@users.noreply.github.com>
2023-12-10 11:23:03 +0000

    minor fix

--- routes/users.go
+++ routes/users.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	controllers "github.com/Slightly-Techie/okr-api/controllers"
+	"github.com/Slightly-Techie/okr-api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	// incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users/:id", controllers.GetUser())
 }
